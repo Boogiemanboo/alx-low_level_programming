1f62ebd2486c0f45b1194c4d0a8ecaad8e5af900
Boogiemanboo <med.am.bekkari@gmail.com>
2023-11-20 02:47:19 -0800

    argc,argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments
  *
- * Return: 0 on success
+ * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
-    (void)argc; /* Unused parameter (argc) */
-    
     printf("%s\n", argv[0]);
-
     return 0;
 }
