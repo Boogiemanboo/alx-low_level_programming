1c9cc94a8a1a5910dd7465cf15f76ae7537b2934
Boogiemanboo <med.am.bekkari@gmail.com>
2023-11-21 03:09:35 -0800

    necessary fixes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,36 +6,34 @@
  * @width: width of the grid
  * @height: height of the grid
  *
- * Return: Pointer to the 2D array, or NULL on failure
+ * Return: pointer to the 2 dimensional array of integers,
+ *         or NULL on failure
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    grid = malloc(height * sizeof(int *));
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
-    if (grid == NULL)
-        return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
 
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(width * sizeof(int));
-
-        if (grid[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return (NULL);
-        }
-
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
-
-    return (grid);
+	return (grid);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid function.
- * @grid: 2D array to free
+ * free_grid - frees a 2 dimensional grid previously created by alloc_grid.
+ * @grid: pointer to the 2 dimensional grid
  * @height: height of the grid
+ *
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    if (grid == NULL || height <= 0)
-        return;
-
-    for (i = 0; i < height; i++)
-        free(grid[i]);
-
-    free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
