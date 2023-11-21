5ba3270726969ae1ce2fbf5ee8d51942b1d58c9d
Boogiemanboo <med.am.bekkari@gmail.com>
2023-11-21 03:03:46 -0800

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,37 +3,42 @@
 
 /**
  * str_concat - concatenates two strings.
- * @s1: first string
- * @s2: second string
+ * @s1: the first string
+ * @s2: the second string
  *
- * Return: Pointer to the newly allocated space containing the concatenated string,
+ * Return: pointer to a newly allocated space in memory containing
+ *         the contents of s1, followed by the contents of s2, null-terminated
  *         or NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int i, j, k, len1 = 0, len2 = 0;
+	char *concatenated;
+	int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 != NULL)
+	{
+		while (s1[len1] != '\0')
+			len1++;
+	}
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	if (s2 != NULL)
+	{
+		while (s2[len2] != '\0')
+			len2++;
+	}
 
-    concat = malloc((len1 + len2 + 1) * sizeof(char));
+	concatenated = malloc((len1 + len2 + 1) * sizeof(char));
 
-    if (concat == NULL)
-        return (NULL);
+	if (concatenated == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		concatenated[i] = s1[i];
 
-    for (j = 0, k = len1; j <= len2; j++, k++)
-        concat[k] = s2[j];
+	for (j = 0; j < len2; j++)
+		concatenated[len1 + j] = s2[j];
 
-    return (concat);
+	concatenated[len1 + len2] = '\0';
+
+	return (concatenated);
 }
