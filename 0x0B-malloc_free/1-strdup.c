bd1c817de8c2a01bf0093314d5fcf251b4a5431d
Boogiemanboo <med.am.bekkari@gmail.com>
2023-11-21 03:00:19 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,24 +11,24 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    unsigned int length = 0, i;
+	char *duplicate;
+	unsigned int length = 0, i;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    /* Calculate the length of the string */
-    while (str[length] != '\0')
-        length++;
+	/* Calculate the length of the string */
+	while (str[length] != '\0')
+		length++;
 
-    duplicate = malloc((length + 1) * sizeof(char));
+	duplicate = malloc((length + 1) * sizeof(char));
 
-    if (duplicate == NULL)
-        return (NULL);
+	if (duplicate == NULL)
+		return (NULL);
 
-    /* Copy the string to the newly allocated memory */
-    for (i = 0; i <= length; i++)
-        duplicate[i] = str[i];
+	/* Copy the string to the newly allocated memory */
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
 
-    return (duplicate);
+	return (duplicate);
 }
